a4fe25586da875c7645a53b68a2eb75523059b96
DGankutsuou <DGankutsuou@github.com>
2025-01-10 14:51:09 +0100

    blessed

--- p26_3caplow.cpp
+++ p26_3caplow.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 string	capitalizer(string str)
 {
-	
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			cout << toupper(str[i]) << endl;
+		
+	}
 }
 
 int	main(void)
