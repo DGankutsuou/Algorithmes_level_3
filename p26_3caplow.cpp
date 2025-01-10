49ff9bf48248d0850aa76030d6dc052aff6a24cd
DGankutsuou <DGankutsuou@github.com>
2025-01-10 14:54:10 +0100

    blessed

--- p26_3caplow.cpp
+++ p26_3caplow.cpp
@@ -2,18 +2,29 @@
 #include <string>
 using namespace std;
 
+string	lowerizer(string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = tolower(str[i]);
+	}
+	return (str);
+}
+
 string	capitalizer(string str)
 {
 	for (int i = 0; i < str.length(); i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			cout << toupper(str[i]) << endl;
-		
+			str[i] = toupper(str[i]);
 	}
+	return (str);
 }
 
 int	main(void)
 {
-
+	cout << capitalizer("abcd efgh ijkl") << endl;
+	cout << lowerizer("ABCD EFGH IJKL") << endl;
 	return (0);
 }
