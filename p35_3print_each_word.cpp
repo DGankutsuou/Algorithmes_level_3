9481f305b0a287d08d250e33b61c3c143c63200c
DGankutsuou <DGankutsuou@github.com>
2025-01-20 16:23:43 +0100

    blessed

--- p35_3print_each_word.cpp
+++ p35_3print_each_word.cpp
@@ -1,6 +1,22 @@
 #include "inputslib.h"
+#include "libft.hpp"
+#include <string>
+#include <vector>
 
 int	main(void)
 {
+	char	*ptr;
+	string	str;
+	char	**words;
+
+	str = input::read_string();
+	ptr = &str[0];
+	words = ft::ft_split(ptr, ' ');
+	cout << "Words of this string:" << endl;
+	while (*words != NULL)
+	{
+		printf ("-> %s\n", *words);
+		words++;
+	}
 	return (0);
 }
