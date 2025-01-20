bd6e98603fc6e5bad71c443d1dbe7362c73c4c44
DGankutsuou <DGankutsuou@github.com>
2025-01-20 18:48:01 +0100

    blessed

---
+++ p39_3strjoin.cpp
@@ -0,0 +1,45 @@
+#include "inputslib.h"
+#include "libft.hpp"
+#include <string>
+#include <vector>
+
+vector <string>	spliter(string str, string delim)
+{
+	string	word;
+	short	pos;
+	vector <string> words;
+
+	while ((pos = str.find(delim)) != string::npos)
+	{
+		word = str.substr(0, pos);
+		if (word != "")
+			words.push_back(word);
+		str.erase(0, pos + delim.length());
+	}
+	if (str != "")
+		words.push_back(word);
+	return (words);
+}
+
+string	joiner(vector <string> words, string delim)
+{
+	string	joined;
+
+	joined = "";
+	for (string &word : words)
+		joined += word + delim;
+	joined.erase(joined.length() - delim.length(), delim.length());
+	return (joined);
+}
+
+int	main(void)
+{
+	string	str;
+	vector <string>	words;
+
+	str = input::read_string();
+	words = spliter(str, " ");
+	cout << "Original string:\n";
+	cout << "-> " << joiner(words, "; ") << endl;
+	return (0);
+}
