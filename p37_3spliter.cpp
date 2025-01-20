8f51d3d63b1ddba1824792c01e6bde5fd4e20884
DGankutsuou <DGankutsuou@github.com>
2025-01-20 17:21:31 +0100

    blessed

---
+++ p37_3spliter.cpp
@@ -0,0 +1,37 @@
+#include "inputslib.h"
+#include "libft.hpp"
+#include <string>
+#include <vector>
+
+vector <string>	spliter(string str, string delim)
+{
+	string	word;
+	short	pos;
+	vector <string> words;
+
+	while ((pos = str.find(delim)) != string::npos)
+	{
+		word = str.substr(0, pos);
+		if (word != "")
+			words.push_back(word);
+		str.erase(0, pos + delim.length());
+	}
+	if (str != "")
+		words.push_back(word);
+	return (words);
+}
+
+int	main(void)
+{
+	string	str;
+	vector <string>	words;
+
+	str = input::read_string();
+	words = spliter(str, " ");
+	cout << "Words are:\n";
+	for (string word : words)
+	{
+		cout << "-> " << word << endl;
+	}
+	return (0);
+}
