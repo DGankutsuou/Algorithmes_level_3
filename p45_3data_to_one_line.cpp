2fc1b1177478e617387ce26f2c85bb1bfe26f6a7
DGankutsuou <DGankutsuou@github.com>
2025-01-23 12:24:14 +0100

    blessed

--- p45_3data_to_one_line.cpp
+++ p45_3data_to_one_line.cpp
@@ -15,7 +15,7 @@ void	read_data(s_data &data)
 	data.acount_number = input::read_string("Enter acount number: ");
 	data.pin_code = input::read_string("Enter pin code: ");
 	data.name = input::read_string("Enter client name: ");
-	data.phone = input::read_string("Enter client phone numer: ");
+	data.phone = input::read_string("Enter client phone number: ");
 	data.acount_balance = input::read_string("Enter acount balance: ");
 }
 
@@ -40,5 +40,6 @@ int	main(void)
 	read_data(data);
 	cout << "\ndata record to be saved\n\n";
 	v_data = put_data_in_vector(data);
-	
+	cout << ft::joiner(v_data, "#//#") << endl;
+	return (0);
 }
