4a198524c58dd09e7c1a085bcd70f01b3199ea09
Ayman Abourizq <aabouriz@c4r2p4.1337.ma>
2025-03-11 21:26:33 +0000

    LINUX

--- bank1.cpp
+++ bank1.cpp
@@ -16,6 +16,74 @@ struct s_data
 	string	acount_balance;
 };
 
+s_data	read_new_client()
+{
+	s_data data;
+
+	cout << "Enter accounnt number: ";
+	getline(cin >> ws, data.acount_number);
+	data.pin_code = input::read_string("Enter pin code: ");
+	data.name = input::read_string("Enter client name: ");
+	data.phone = input::read_string("Enter client phone number: ");
+	data.acount_balance = input::read_string("Enter acount balance: ");
+	return (data);
+}
+
+string	record_data(s_data &data)
+{
+	string	record;
+	string	delim;
+
+	record = "";
+	delim = "#//#";
+	record += data.acount_number + delim;
+	record += data.pin_code + delim;
+	record += data.name + delim;
+	record += data.phone + delim;
+	record += data.acount_balance;
+	return (record);
+}
+
+void	save_record_to_file(string file_name, string record)
+{
+	fstream	file;
+
+	file.open(FILE_NAME, ios::app);
+	if (file.is_open())
+	{
+		file << record << endl;
+		file.close();
+	}
+	else
+	{
+		cerr << "Failed to write in file :(" << endl;
+		exit(1);
+	}
+}
+
+void	add_new_client()
+{
+	s_data	data;
+
+	data = read_new_client();
+	save_record_to_file(FILE_NAME, record_data(data));
+}
+
+void	add_clients()
+{
+	char	add_more;
+
+	add_more = 'y';
+	do
+	{
+		system("clear");
+		cout << "\n*Adding new client*\n\n";
+		add_new_client();
+		cout << "\nClient added successfuly. Dost Thou desire to add more (Y/N)?\n-> ";
+		cin >> add_more;
+	} while (tolower(add_more) == 'y');
+}
+
 s_data	split_record(string str)
 {
 	s_data	data;
@@ -76,6 +144,7 @@ void	line_printer(string &line)
 
 void	print_clients_table(vector <string> &v_file)
 {
+	system("clear");
 	print_header(v_file.size());
 	for (string &line : v_file)
 	{
@@ -97,11 +166,18 @@ void	act(short choise)
 	{
 		case 1:
 			print_clients_table(v_file);
-		// case 2:
+			break ;
+		case 2:
+			add_clients();
+			break ;
 		// case 3:
+			break ;
 		// case 4:
+			break ;
 		// case 5:
-		// default:
+			break ;
+		default:
+			exit (0);
 	}
 }
 
@@ -131,9 +207,7 @@ void	bank_system()
 		choise = input::read_number_in_range(1, 6);
 		act(choise);
 		cout << "\nPress any key to return into main menu...";
-		cin.clear();
-		cin.get();
-		break ;
+		cin >> pause;
 	}
 }
 
